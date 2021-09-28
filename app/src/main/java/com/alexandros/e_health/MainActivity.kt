726e9072a97d8d8a8426df08ead5cdb203f94b4c
Konstantinoc <kotchios99@gmail.com>
2021-09-28 11:49:50 +0300

    add RegisterUI add functionality on label Dont have an account add functionality on labels Already have an accout

--- app/src/main/java/com/alexandros/e_health/MainActivity.kt
+++ app/src/main/java/com/alexandros/e_health/MainActivity.kt
@@ -2,10 +2,21 @@ package com.alexandros.e_health
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtRegister.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/alexandros/e_health/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.alexandros.e_health
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        tvLogin.setOnClickListener(){
+            this.finish()
+        }
+    }
+}
