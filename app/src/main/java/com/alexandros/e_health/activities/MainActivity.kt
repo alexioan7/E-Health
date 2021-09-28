526f17b0a44daf0e772f4e003e4c1edc123c7a67
Alexandros Ioannou <alexioan92@gmail.com>
2021-09-28 11:46:24 +0300

    project structure

--- app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
+++ app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.alexandros.e_health
+package com.alexandros.e_health.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alexandros.e_health.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
