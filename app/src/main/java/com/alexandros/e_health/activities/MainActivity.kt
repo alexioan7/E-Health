9a95b97a1f1c5a1094d19c1e0b1f1f6d58c222f8
Konstantinoc <kotchios99@gmail.com>
2021-09-28 13:53:17 +0300

    remove setOnClickListener from MainActivity.kt

--- app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
+++ app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
@@ -10,10 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        tvRegister.setOnClickListener(){
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
