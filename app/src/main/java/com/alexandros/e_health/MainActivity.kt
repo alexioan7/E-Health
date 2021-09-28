60e285ea8346c8d4ea761492265086ac2e55bdcc
Konstantinoc <kotchios99@gmail.com>
2021-09-28 12:07:53 +0300

    reverse MainActivity.kt

--- app/src/main/java/com/alexandros/e_health/MainActivity.kt
+++ app/src/main/java/com/alexandros/e_health/MainActivity.kt
@@ -1,22 +1,12 @@
-package com.alexandros.e_health
+package com.alexandros.e_health.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.Intent
-
-
-
+import com.alexandros.e_health.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txtRegister.setOnClickListener(){
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
