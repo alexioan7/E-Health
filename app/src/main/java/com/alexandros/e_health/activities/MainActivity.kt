e57472ede7f4f1b7d5114881ebb55c19d1f1596a
Konstantinoc <kotchios99@gmail.com>
2021-09-28 14:00:53 +0300

    remove unused lines from MainActivity.kt

--- app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
+++ app/src/main/java/com/alexandros/e_health/activities/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.alexandros.e_health.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alexandros.e_health.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
