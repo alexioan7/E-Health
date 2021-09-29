72c1d0eef875e78403b9e532c89d74c4daa89e97
Alexandros Ioannou <alexioan92@gmail.com>
2021-09-29 10:14:27 +0300

    AuthApi interface

--- app/src/main/java/com/alexandros/e_health/api/AuthApi.kt
+++ app/src/main/java/com/alexandros/e_health/api/AuthApi.kt
@@ -1,6 +1,19 @@
 package com.alexandros.e_health.api
 
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface AuthApi {
 
-
+    @FormUrlEncoded
+    @POST("users")
+    fun registerUser(
+        @Field("amka") amka: String,
+        @Field("password") password: String,
+        @Field("name") name: String,
+        @Field("surname") surname: String,
+        @Field("email") email: String,
+        @Field("phone number") phoneNumber: Int
+    ) : Any
 }
