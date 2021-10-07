d658bfdf6ba2010cd2a247e18a01609dd55670bb
Konstantinoc <kotchios99@gmail.com>
2021-10-07 17:52:35 +0300

    bottom nav menu, now you can change fragment

--- app/src/main/java/com/alexandros/e_health/activities/DiagnosesFragment.kt
+++ app/src/main/java/com/alexandros/e_health/activities/DiagnosesFragment.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.alexandros.e_health.R
 import com.alexandros.e_health.databinding.FragmentDiagnosesBinding
 import com.alexandros.e_health.viewmodels.DiagnosesViewModel
 
-class DiagnosesFragment : Fragment() {
+class DiagnosesFragment : Fragment(R.layout.fragment_diagnoses) {
 
     private lateinit var binding: FragmentDiagnosesBinding
     private lateinit var viewmodel: DiagnosesViewModel
