93fbb7737b3d96bf30bdfcd67956322a54075549
Alexandros Ioannou <alexioan92@gmail.com>
2021-10-15 16:34:35 +0300

    little typo change

---
+++ app/src/main/java/com/alexandros/e_health/api/AuthApiInterface.kt
@@ -0,0 +1,25 @@
+package com.alexandros.e_health.api
+
+
+import com.alexandros.e_health.api.responseModel.LoginBody
+import com.alexandros.e_health.api.responseModel.LoginUserResponse
+import com.alexandros.e_health.api.responseModel.RegisterBody
+import com.alexandros.e_health.api.responseModel.RegisterUserResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApiInterface {
+
+
+    @POST("users/signup")
+    fun registerUser(
+        @Body body: RegisterBody
+    ): Call<RegisterUserResponse>
+
+    @POST("users/login")
+    fun loginUser(
+        @Body body: LoginBody
+    ): Call<LoginUserResponse>
+
+}
