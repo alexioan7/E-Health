665b579e1ec41bf89d91dd2407a6804f388c2e98
dimitra97 <63058524+dimitra97@users.noreply.github.com>
2021-10-18 23:15:00 +0300

    prescriptions

--- app/src/main/java/com/alexandros/e_health/viewmodels/PrescriptionsViewModel.kt
+++ app/src/main/java/com/alexandros/e_health/viewmodels/PrescriptionsViewModel.kt
@@ -1,13 +1,32 @@
 package com.alexandros.e_health.viewmodels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.alexandros.e_health.api.responseModel.Doctor
+import com.alexandros.e_health.api.responseModel.Hospital
+import com.alexandros.e_health.api.responseModel.PrescriptionsUserResponse
+import com.alexandros.e_health.repositories.AuthRepository
 
-class PrescriptionsViewModel : ViewModel() {
+class PrescriptionsViewModel (private val authRepo: AuthRepository): ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    val medicine: String? = null
+    val dispensed: Boolean = false
+    val createdAt: String? = null
+    val hospital: Hospital? = null
+    val doctor: Doctor? = null
+    val description: String? = null
+    val active: Boolean = false
+
+    fun getUserPrescriptionsFromRepo(): MutableLiveData<List<PrescriptionsUserResponse>> {
+        return authRepo.userPrescriptionsFromRemoteData
     }
-    val text: LiveData<String> = _text
+
+    fun requestUserPrescriptions(){
+        authRepo.requestPrescriptions()
+    }
+
+
+
+
+
 }
