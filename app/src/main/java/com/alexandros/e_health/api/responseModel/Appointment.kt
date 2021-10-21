fb95b1af3a91f267c93b25247cdd9e3fbd8d00cd
Alexandros Ioannou <alexioan92@gmail.com>
2021-10-21 13:16:06 +0300

    apointment card layout desigin.

--- app/src/main/java/com/alexandros/e_health/api/responseModel/Appointment.kt
+++ app/src/main/java/com/alexandros/e_health/api/responseModel/Appointment.kt
@@ -4,7 +4,7 @@ data class Appointment(
     val _id: String,
     val active: Boolean,
     val date: String,
-    val department: DepartmentX,
+    val department: Department,
     val id: String,
     val user: String
 )
