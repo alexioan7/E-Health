c371195df3e44404a27a276e12e95f83da03ef0b
Alexandros Ioannou <alexioan92@gmail.com>
2021-10-25 00:45:25 +0300

    create appointment fragment

---
+++ app/src/main/java/com/alexandros/e_health/activities/CreateAppointmentFragment.kt
@@ -0,0 +1,16 @@
+package com.alexandros.e_health.activities
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.alexandros.e_health.R
+
+
+class CreateAppointmentFragment : Fragment(R.layout.fragment_create_appointment) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
